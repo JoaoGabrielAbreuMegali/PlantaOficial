938ff1b4220bb738fcd408102d7b9ffc943e090a
Joao Gabriel Abreu Megali <jgmegali@gmail.com>
2025-02-10 18:51:58 -0300

    atualização da planta

--- CodigoVT.m
+++ CodigoVT.m
@@ -4,7 +4,10 @@ clc
 clear
 
 
-VelRef = 30;                %[m/s]
+AirTemperature = 303; %[K]
+VelRef = 30;          %[m/s]
+
+
 EV.FrontAxlePositionfromCG = 0.775;
 EV.RearAxlePositionfromCG = 0.775;
 EV.CornerStiffFront =   50420.286;        %[N/rad]
@@ -16,8 +19,8 @@ EV.WheelRadius = 0.30;
 EV.GearRatio = 8.89;
 
 
-MTR.Speed  = [0 461 565 670 775 880 942 943];
-MTR.Torque = [13.1	13.1	10.6	9.0     7.7     6.8     6.4     0.1];
+MTR.Speed  = [0 461 565 670 775 880 942 943]; %rad/s = rpm . (2pi/60)
+MTR.Torque = [13.1	13.1	10.6	9.0     7.7     6.8     6.4     0.1]; %N.m
 MTR.Overpower = 3.5;
 MTR.MaxTorque = 13.1*MTR.Overpower;
 
